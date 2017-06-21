454ab582290160b5b3ee288a924af0e0542072e8
keenwon <semanwmj@gmail.com>
2017-06-22 01:02:55 +0800

    添加 eslint

---
+++ test/eslint.spec.js
@@ -0,0 +1,17 @@
+'use strict';
+
+const lint = require('mocha-eslint');
+
+let paths = [
+  'index.js'
+];
+
+let options = {
+    formatter: 'compact',
+    alwaysWarn: false,
+    timeout: 5000,
+    slow: 1000,
+    strict: true
+};
+
+lint(paths, options);
