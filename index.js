fc38be3348d19f1e8df73c8bd6d3870ac214083f
keenwon <semanwmj@gmail.com>
2017-06-24 17:45:22 +0800

    调整eslint配置

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 const fetch = require('node-fetch');
 const { JSDOM } = require('jsdom');
 
-function parser (html) {
+function parser(html) {
   let dom = new JSDOM(html);
   let $containor = dom.window.document.querySelector('#phrsListTab');
 
@@ -17,7 +17,7 @@ function parser (html) {
     );
   });
 
-  // 提取翻译 
+  // 提取翻译
   let trans = [];
   $containor.querySelectorAll('li').forEach(item => {
     trans.push(item.textContent);
@@ -31,6 +31,7 @@ function parser (html) {
 
 function main(keyword) {
   // 模拟浏览器的头信息
+  /* eslint-disable max-len */
   const headers = new fetch.Headers({
     'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
     'Accept-Encoding': 'gzip, deflate',
@@ -41,6 +42,7 @@ function main(keyword) {
     'Upgrade-Insecure-Requests': '1',
     'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.104 Safari/537.36'
   });
+  /* eslint-enable max-len */
 
   const url = `http://www.youdao.com/w/eng/${keyword}`;
 
@@ -48,6 +50,7 @@ function main(keyword) {
     .then(res => res.text())
     .then(body => parser(body))
     .then(data => {
+      // eslint-disable-next-line
       console.log(data);
     });
 }
